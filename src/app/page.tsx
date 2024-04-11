732874ed8d65633979e689aaff867b2eac2962e6
choichoijin <cuj10594@naver.com>
2024-04-11 10:09:01 +0900

    feat: add sample page with styled components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,27 @@
 "use client";
 
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 import styled from "styled-components";
 
 export default function Home() {
+  const router = useRouter();
+
+  const goNewPage = () => {
+    router.push("/survey/new");
+  };
+
   return (
     <main>
       <div>
-        <Test>Main Page</Test>
+        <StButton onClick={goNewPage}>설문지 만들러가기</StButton>
       </div>
     </main>
   );
 }
 
-const Test = styled.div`
+const StButton = styled.button`
   font-size: 20px;
-  color: blue;
+  color: black;
+  background-color: white;
 `;
---
+++ src/app/survey/new/page.tsx
@@ -0,0 +1,5 @@
+import styled from "styled-components";
+
+export default function New() {
+  return <div>new page</div>;
+}
